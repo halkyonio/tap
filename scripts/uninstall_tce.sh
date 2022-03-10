a72fac133f8532a6cfde1ded4130ceb351795168
Charles Moulliard <cmoulliard@redhat.com>
2022-03-10 10:34:14 +0100

    Refactor the uninstall script

--- scripts/uninstall_tce.sh
+++ scripts/uninstall_tce.sh
@@ -2,8 +2,40 @@
 #
 # Execute this command locally
 #
-# ./install_tce.sh
+# ./uninstall_tce.sh
 
+set -e
 
-IP=65.108.148.216
-tanzu uc delete toto
+# Defining some colors for output
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+
+repeat_char(){
+  COLOR=${1}
+	for i in {1..50}; do echo -ne "${!COLOR}$2${NC}"; done
+}
+
+log_msg() {
+    COLOR=${1}
+      MSG="${@:2}"
+    echo -e "\n${!COLOR}## ${MSG}${NC}"
+}
+
+log() {
+  MSG="${@:2}"
+  echo; repeat_char ${1} '#'; log_msg ${1} ${MSG}; repeat_char ${1} '#'; echo
+}
+
+KUBE_CFG_FILE=${1:-config}
+export KUBECONFIG=$HOME/.kube/${KUBE_CFG_FILE}
+
+CLUSTER_NAME=${CLUSTER_NAME:-toto}
+
+log "YELLOW" "Deleting the TCE cluster $CLUSTER_NAME"
+tanzu uc delete $CLUSTER_NAME
