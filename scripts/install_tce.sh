718d56ec302db7d062dd34a9be9a6aa6912c9d42
Charles Moulliard <cmoulliard@redhat.com>
2022-03-10 13:47:46 +0100

    Use string instead of a var and fix wrong VAR used. IP --> VM_IP

--- scripts/install_tce.sh
+++ scripts/install_tce.sh
@@ -208,19 +208,19 @@ kubectl create serviceaccount dashboard -n kubernetes-dashboard
 kubectl create clusterrolebinding dashboard-admin -n kubernetes-dashboard --clusterrole=cluster-admin --serviceaccount=kubernetes-dashboard:dashboard
 
 K8s_TOKEN=$(kubectl get secret $(kubectl get serviceaccount dashboard -n kubernetes-dashboard -o jsonpath="{.secrets[0].name}") -o jsonpath="{.data.token}" -n kubernetes-dashboard | base64 --decode)
-log_line "YELLOW" "Kubernetes dashboard URL: https://k8S-ui.$IP.nip.io"
+log_line "YELLOW" "Kubernetes dashboard URL: https://k8S-ui.$VM_IP.nip.io"
 log_line "YELLOW" "Kubernetes dashboard TOKEN: $K8s_TOKEN"
 
 HARBOR_PWD_STR=$(cat $TCE_DIR/values-harbor.yml | grep harborAdminPassword)
 IFS=': ' && read -a strarr <<< $HARBOR_PWD_STR
 HARBOR_PWD=${strarr[1]}
-log "YELLOW" "Harbor URL: https://harbor.$IP.nip.io and admin password: $HARBOR_PWD"
+log "YELLOW" "Harbor URL: https://harbor.$VM_IP.nip.io and admin password: $HARBOR_PWD"
 
 log_line "YELLOW" "To push/pull images from the Harbor registry, create a secret and configure the imgPullSecret of the service account"
 log_line "YELLOW" "kubectl -n <NAMESPACE> create secret docker-registry regcred \""
 log_line "YELLOW" "    --docker-server=harbor.<IP>.nip.io \""
 log_line "YELLOW" "    --docker-username=admin \""
-log_line "YELLOW" "    --docker-password=$HARBOR_PWD"
+log_line "YELLOW" "    --docker-password=<HARBOR_PWD>"
 log_line "YELLOW" "kubectl patch serviceaccount default -n <NAMESPACE> -p '{"imagePullSecrets": [{"name": "regcred"}]}'"
 
 ELAPSED="Elapsed: $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec" && echo $ELAPSED
