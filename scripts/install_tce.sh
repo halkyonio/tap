48010c4c65dc2675968ca5e6afeb044c33c0d265
Charles Moulliard <cmoulliard@redhat.com>
2022-03-11 15:51:32 +0100

    Revert code to generate the X509 certificate

--- scripts/install_tce.sh
+++ scripts/install_tce.sh
@@ -95,59 +95,59 @@ log "CYAN" "Generate the openssl stuff"
 TCE_DIR=$HOME/tce
 
 # Generate a CA certificate private key.
-openssl genrsa -out $TCE_DIR/certs/ca.key 4096
+#openssl genrsa -out $TCE_DIR/certs/ca.key 4096
 
 # Generate the CA certificate.
-openssl req -x509 -new -nodes -sha512 -days 3650 \
- -subj "/C=CN/ST=Namur/L=Florennes/O=Red Hat/OU=Snowdrop/CN=harbor.65.108.148.216.nip.io" \
- -key $TCE_DIR/certs/ca.key \
- -out $TCE_DIR/certs/ca.crt
+#openssl req -x509 -new -nodes -sha512 -days 3650 \
+# -subj "/C=CN/ST=Namur/L=Florennes/O=Red Hat/OU=Snowdrop/CN=harbor.65.108.148.216.nip.io" \
+# -key $TCE_DIR/certs/ca.key \
+# -out $TCE_DIR/certs/ca.crt
 
 # Generate a Server Certificate
-openssl genrsa -out tls.key 4096
-openssl req -sha512 -new \
-    -subj "/C=CN/ST=Namur/L=Florennes/O=Red Hat/OU=Snowdrop/CN=harbor.65.108.148.216.nip.io" \
-    -key $TCE_DIR/certs/tls.key \
-    -out $TCE_DIR/certs/tls.csr
+#openssl genrsa -out tls.key 4096
+#openssl req -sha512 -new \
+#    -subj "/C=CN/ST=Namur/L=Florennes/O=Red Hat/OU=Snowdrop/CN=harbor.65.108.148.216.nip.io" \
+#    -key $TCE_DIR/certs/tls.key \
+#    -out $TCE_DIR/certs/tls.csr
 
 # Generate an x509 v3 extension file.
-cat > $TCE_DIR/certs/v3.ext <<-EOF
-basicConstraints        = critical, CA:TRUE
-subjectKeyIdentifier    = hash
-authorityKeyIdentifier  = keyid:always, issuer:always
-keyUsage                = critical, cRLSign, digitalSignature, keyCertSign
-nsComment               = "OpenSSL Generated Certificate"
-subjectAltName          = @alt_names
-
-[alt_names]
-DNS.1=harbor.65.108.148.216.nip.io
-DNS.2=notary.harbor.65.108.148.216.nip.io
-EOF
+# cat > $TCE_DIR/certs/v3.ext <<-EOF
+# basicConstraints        = critical, CA:TRUE
+# subjectKeyIdentifier    = hash
+# authorityKeyIdentifier  = keyid:always, issuer:always
+# keyUsage                = critical, cRLSign, digitalSignature, keyCertSign
+# nsComment               = "OpenSSL Generated Certificate"
+# subjectAltName          = @alt_names
+#
+#[alt_names]
+#DNS.1=harbor.65.108.148.216.nip.io
+#DNS.2=notary.harbor.65.108.148.216.nip.io
+#EOF
 
 # Use the v3.ext file to generate a certificate for your Harbor host.
-openssl x509 -req -sha512 -days 3650 \
-    -extfile $TCE_DIR/certs/v3.ext \
-    -CA $TCE_DIR/certs/ca.crt -CAkey $TCE_DIR/certs/ca.key -CAcreateserial \
-    -in $TCE_DIR/certs/tls.csr \
-    -out $TCE_DIR/certs/tls.crt
+#openssl x509 -req -sha512 -days 3650 \
+#    -extfile $TCE_DIR/certs/v3.ext \
+#    -CA $TCE_DIR/certs/ca.crt -CAkey $TCE_DIR/certs/ca.key -CAcreateserial \
+#    -in $TCE_DIR/certs/tls.csr \
+#    -out $TCE_DIR/certs/tls.crt
 
-mkdir -p $TCE_DIR/certs/${REG_SERVER}
-cp $TCE_DIR/certs/ca.crt $TCE_DIR/certs/${REG_SERVER}
-cp $TCE_DIR/certs/tls.crt $TCE_DIR/certs/${REG_SERVER}
-cp $TCE_DIR/certs/tls.key $TCE_DIR/certs/${REG_SERVER}
+# mkdir -p $TCE_DIR/certs/${REG_SERVER}
+# cp $TCE_DIR/certs/ca.crt $TCE_DIR/certs/${REG_SERVER}
+# cp $TCE_DIR/certs/tls.crt $TCE_DIR/certs/${REG_SERVER}
+# cp $TCE_DIR/certs/tls.key $TCE_DIR/certs/${REG_SERVER}
 
 
-# create_openssl_cfg > $TCE_DIR/certs/req.cnf
+create_openssl_cfg > $TCE_DIR/certs/req.cnf
 
-# log "CYAN" "Create the self signed certificate certificate and client key files"
-# openssl req -x509 \
-#   -nodes \
-#   -days 365 \
-#   -newkey rsa:4096 \
-#   -keyout $TCE_DIR/certs/${REG_SERVER}/tls.key \
-#   -out $TCE_DIR/certs/${REG_SERVER}/tls.crt \
-#   -config $TCE_DIR/certs/req.cnf \
-#   -sha256
+log "CYAN" "Create the self signed certificate certificate and client key files"
+openssl req -x509 \
+  -nodes \
+  -days 365 \
+  -newkey rsa:4096 \
+  -keyout $TCE_DIR/certs/${REG_SERVER}/tls.key \
+  -out $TCE_DIR/certs/${REG_SERVER}/tls.crt \
+  -config $TCE_DIR/certs/req.cnf \
+  -sha256
 
 log "CYAN" "Configure the TCE cluster config file: $TCE_DIR/config.yml"
 cat <<EOF > $TCE_DIR/config.yml
