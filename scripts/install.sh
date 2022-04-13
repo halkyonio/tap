ccb50a100743ddb014f297e690672f58101de17f
Charles Moulliard <cmoulliard@redhat.com>
2022-04-13 18:53:31 +0200

    The version of the client must also be bumped

--- scripts/install.sh
+++ scripts/install.sh
@@ -85,7 +85,7 @@ NAMESPACE_TAP="tap-install"
 PIVNET_CLI_VERSION="3.0.1"
 TANZU_CLUSTER_ESSENTIALS_VERSION="1.1.0"
 TAP_VERSION="1.1.0"
-TANZU_CLI_VERSION="v0.11.1"
+TANZU_CLI_VERSION="v0.11.2"
 
 # Do not use the RAW URL but instead the Github HTTPS URL followed by blob/main
 TAP_GIT_CATALOG_REPO=https://github.com/halkyonio/tap-catalog-blank/blob/main
