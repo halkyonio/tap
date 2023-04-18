e814b23100e28f469bc66ea092afaf40988b591a
Charles Moulliard <cmoulliard@redhat.com>
2023-04-18 13:13:07 +0200

    Suppress error messag whn packages are installed
    Signed-off-by: Charles Moulliard <cmoulliard@redhat.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -477,7 +477,7 @@ installTapPackages() {
     --wait-check-interval 10 \
     -v ${TAP_VERSION} \
     --values-file tap-values.yml \
-    -n ${NAMESPACE_TAP}
+    -n ${NAMESPACE_TAP} >/dev/null 2>&1
 
   log "CYAN" "Wait till TAP installation is over"
   resp=$(tanzu package installed get tap -n ${NAMESPACE_TAP} -o json | jq -r .[].status)
